64bdfbf0df753a113ef890522cb0a66dd9266638
光城 桶谷 <h1202016015@hiratagakuen.onmicrosoft.com>
2020-10-20 14:46:49 +0900

    winsceneのコピーしました

---
+++ Project1/Project1/winscene.cpp
@@ -0,0 +1,41 @@
+//STLデバッグ機能をオフにする。
+#define _SECURE_SCL(o)
+#define _HAS_ITERATOR_DEBUGGING(o)
+
+//GameL使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawFont.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include "Winscene.h"
+#include"GameHead.h"
+
+//コンストラクタ
+CSceneWin::CSceneWin()
+{
+
+}
+
+//デストラクタ
+CSceneWin::~CSceneWin()
+{
+
+}
+
+//初期化メソッド
+//文字の登録
+void CSceneWin::InitScene()
+{
+	Font::SetStrTex(L"You Win!");
+	Font::SetStrTex(L"エンターキーでタイトルに戻る。");
+
+}
+
+//実行メソッド
+void CSceneWin::Scene()
+{
+
+}
