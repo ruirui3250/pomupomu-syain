6e7cdddff4098e22d1ef5907bf3bddace24f1c1c
qimr98159@mineo.jp <qimr98159@mineo.jp>
2020-10-22 15:45:01 +0900

    blockfeblinという障害物オブジェクトの途中でございます。

---
+++ Project1/Project1/blockfeblin.cpp
@@ -0,0 +1,27 @@
+//使用するヘッダーファイル
+#include "GameL\DrawTexture.h"
+#include"GameL\WinInputs.h"
+#include "GameL\SceneManager.h"
+
+#include"GameHead.h"
+#include"blockfeblin.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjBlockfeblin::Init()
+{
+
+}
+//アクション
+void CObjBlockfeblin::Action()
+{
+
+}
+
+//ドロー
+void CObjBlockfeblin::Draw()
+{
+
+}
---
+++ Project1/Project1/blockfeblin.h
@@ -0,0 +1,19 @@
+#pragma once
+//使用するヘッダー
+#include "GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクトブロック＆背景
+class CObjBlockfeblin :public CObj
+{
+public:
+	CObjBlockfeblin() {};
+	~CObjBlockfeblin() {};
+	void Init();
+	void Action();
+	void Draw();//ドロー
+private:
+	//マップ情報
+	int blockfeblin_data[10][10];
+};
